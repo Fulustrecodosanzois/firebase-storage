aa02eb74262af7dea7c4fa221e5a7b415c3e0579
Luciano Silva <luciano_jet@hotmail.com>
2023-11-10 21:22:04 -0300

    configuração enviando simples, mas estavel

---
+++ js/app.js
@@ -0,0 +1,138 @@
+import {app, db} from "./config-firebase.js"
+import { getStorage, ref, uploadBytes, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js"
+import { collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js"
+
+
+let imagem = document.querySelector("#arquivo")
+let btnEnviar = document.querySelector("#btnEnviar")
+let blocoImagens = document.querySelector("#blocoImagens")
+let btnCarregar = document.querySelector("#btnCarregar")
+
+let timestamp = new Date().getTime() // para poder ser utilizado como nome da imagem ao salvar no cloud storage.
+
+let dadosImagem = {
+    nome:"",
+    url: "",
+    timestamp: ""
+}
+
+
+const storage = getStorage(app);
+const storageRef = ref(storage, `imagem/${timestamp}`);// função ref() recebe a instância de armazenamento storage e uma string que representa o caminho para o local desejado, seria o nome do arquivo lá no storage. Estou salvando dentro de uma pasta chamado 'imagem/' e em seguida
+
+
+
+// SALVANDO IMAGEM NO STORAGE E NO LOCALSTORAGE
+function store(){
+    try {        
+        //console.log(imagem.files[0])
+
+        dadosImagem.nome = imagem.files[0].name// salvando o nome da imagem
+        let data = new Date(timestamp)// convertendo o timestamp para data e hora
+        dadosImagem.timestamp = data.toLocaleString("pt-BR")// alterando a data e hora para o padrão BR
+
+      
+            // Fazendo o upload da imagem para o Storage
+            uploadBytes(storageRef, imagem.files[0]).then((resultado) => {
+                //console.log('Upload realizado', resultado);
+
+                // Obtendo a URL de download da imagem
+                getDownloadURL(resultado.ref)
+                .then((url) => {
+
+                    //console.log(url)
+                    dadosImagem.url = url
+
+                    cadastrarImagem(dadosImagem)
+                })
+                .catch((error) => {
+                    // Uma lista completa de códigos de erro está disponível em
+                    // https://firebase.google.com/docs/storage/web/handle-errors
+                    console.log("Erro ao gerar a URL da imagem: "+error)
+                    switch (error.code) {
+                    case 'storage/object-not-found':
+                        // File doesn't exist
+                        break;
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+            
+                    // ...
+            
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect the server response
+                        break;
+                    }
+                });            
+                
+            });            
+           
+               
+    } catch (error) {
+        console.error("Erro ao fazer upload da imagem: ", error);
+        alert("Não foi possível fazer o upload da imagem, insira uma imagem válida")
+    }
+}
+
+// CADASTRANDO A IMAGEM NO FIRESTORE
+async function cadastrarImagem(dados){
+    try {
+        const docRef = await addDoc(collection(db, "imagens"), dados);
+        //console.log("Document criado com ID: ", docRef.id);
+        alert("Imagem cadastrada com sucesso");
+        
+        //resetar() 
+
+
+    } catch (e) {
+        alert("Não foi possível cadastrar a imagem")
+        console.error("Erro ao criar o documento: ", e);
+    }
+    
+    //getDados() // chamando função para atualizar lista de dados ao criar novo contato
+}
+
+//RECUPERANDO DADOS DO FIREBASE - FIRESTORE
+async function getDados(){
+    blocoImagens.innerHTML = ""// limpando o elemento html antes de inserir novos dados, caso contrário, irá acumular vários valores
+
+    const busca = query(collection(db, "imagens"), orderBy("nome"));// ordenando a exibição dos dados em ordem alfabética
+
+    const consulta = await getDocs(busca);
+    consulta.forEach((itens) => {
+        //console.log(itens.data())
+       //console.log(`${itens.id} => ${itens.data()}`);// Todos os dados
+       // console.log(`Nome: ${itens.data().nome}\nEmail: ${itens.data().email}`)// Dados específicos
+
+       blocoImagens.innerHTML += `
+       <div class="col">
+            <div class="card h-100">
+                <img src="${itens.data().url}" class="card-img-top tamanho mx-auto p-2" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${itens.data().nome}</h5>
+                    <p class="card-text"><strong>Data de inserção no banco:</strong>${itens.data().timestamp}</p>
+                </div>
+            </div>  
+        </div>   
+        `
+
+    });
+}
+
+btnEnviar.addEventListener("click", (evento)=>{
+    evento.preventDefault()
+
+    store()    
+
+})
+
+btnCarregar.addEventListener("click",(evento)=>{
+    evento.preventDefault()
+
+    getDados()
+})
+
+getDados()
---
+++ js/local-storage.js
@@ -0,0 +1,86 @@
+let imagem = document.querySelector("#arquivo")
+let btnEnviar = document.querySelector("#btnEnviar")
+let blocoImagens = document.querySelector("#blocoImagens")
+let btnCarregar = document.querySelector("#btnCarregar")
+
+let listaImagens = []
+const dadosImagem = {
+    nome:"",
+    url:""
+}
+
+/* SALVANDO DADOS NO LOCALSTORAGE - LOCALSTORAGE LIMITA A 10MB */
+function salvarImagem(){
+    
+    console.log(imagem.files[0])
+   
+    
+    dadosImagem.url = imagem.files[0]
+    dadosImagem.nome = imagem.files[0].name
+
+    let recebeImagem = JSON.parse(localStorage.getItem("listaImagens"))
+
+    if(recebeImagem == null){
+        console.log("Sem nada por enquanto")
+
+        listaImagens.push(dadosImagem)
+        
+        localStorage.setItem("listaImagens",JSON.stringify(listaImagens))
+    }
+    else{
+
+        listaImagens = recebeImagem
+
+        listaImagens.push(dadosImagem)            
+        localStorage.setItem("listaImagens",JSON.stringify(listaImagens))
+
+        console.log(listaImagens)
+
+    }
+    // é preciso adicionar um evento de 'load' para garantir que as informações do arquivo sejam carregadas de forma correta antes de exibir o conteúdo do arquivo que foi feito o upload
+
+}
+function carregarImagem(){
+    
+    let listaImagens = JSON.parse(localStorage.getItem("listaImagens"))
+
+    /*
+
+    //console.log(listaImagens)
+    */
+
+    
+    for(itens of listaImagens){
+        console.log(itens)
+        let card = document.createElement("div")
+        card.classList.add("col")
+        card.innerHTML = `
+
+        <div class="card h-100">
+          <img src="${itens.url}" class="card-img-top" alt="..." id="imgCard">
+          <div class="card-body">
+            <h5 class="card-title">${itens.nome}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+
+        `
+        blocoImagens.appendChild(card)
+    }
+
+}
+
+
+btnEnviar.addEventListener("click", (evento)=>{
+    evento.preventDefault()
+
+    salvarImagem()  
+
+})
+
+btnCarregar.addEventListener("click",(evento)=>{
+    evento.preventDefault()
+
+   // carregarImagem()
+
+})
